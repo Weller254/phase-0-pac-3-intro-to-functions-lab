5be1f046b7e4cc201fdb74a0409a48aa0626fa60
Kelvin Muturi <muturikelvin893@gmail.com>
2023-04-11 12:58:59 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,21 @@
+
+function shout(string) {
+    return string.toUpperCase();
+  }
+function whisper(string){
+    return string.toLowerCase();
+  }
+function logShout(hello){
+    console.log("HELLO");
+  }
+function logWhisper (hello){
+    console.log("hello");
+  }
+function sayHiToHeadphonedRoommate(hello){    
+    return ("I can't hear you!");    
+}
+function sayHiToHeadphonedRoommate(HELLO){
+    return ("YES INDEED!");
+}
+function sayHiToHeadphonedRoommate(string)
+    return ("I would love to!");
